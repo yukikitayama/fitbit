d9273f20cf1db1782c187fba23dad7299ff9ae91
Yuki Kitayama <yk2797@columbia.edu>
2021-11-30 18:27:43 -0700

    made code to get heart rate intraday time series data for a specific date

---
+++ get_heart_rate_time_series_by_date.py
@@ -0,0 +1,50 @@
+import boto3
+import requests
+import json
+import pprint
+
+
+SECRET_NAME = 'fitbit'
+REGION_NAME = 'us-west-1'
+DATE = '2021-11-27'  # Saturday, first day the heart rate data is available through the day
+RESPONSE_JSON = f'data/heart_rate_time_series_{DATE}.json'
+PERIOD = '1d'  # Supported: 1d | 7d | 30d | 1w | 1m
+
+
+def get_secret(region_name: str, secret_name: str) -> dict:
+    session = boto3.session.Session()
+    client = session.client(service_name='secretsmanager', region_name=region_name)
+    get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    secret_string = get_secret_value_response['SecretString']
+    secret = json.loads(secret_string)
+    return secret
+
+
+def main():
+
+    # Get secrets
+    secret = get_secret(region_name=REGION_NAME, secret_name=SECRET_NAME)
+    access_token = secret['access-token']
+    encoded_id = secret['encoded-id']
+
+    # Get sleep log data by date
+    url = f'https://api.fitbit.com/1/user/{encoded_id}/activities/heart/date/{DATE}/{PERIOD}.json'
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    response = requests.request('GET', url, headers=headers)
+    print('Response headers')
+    pprint.pprint(response.headers)
+    print()
+    print(f'Response contents')
+    pprint.pprint(response.json())
+    print()
+
+    # Save data
+    with open(RESPONSE_JSON, 'w') as outfile:
+        json.dump(response.json(), outfile, indent=2)
+    print(f'Saved the data from fitbit API to {RESPONSE_JSON}')
+
+
+if __name__ == '__main__':
+    main()
